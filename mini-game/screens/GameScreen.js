f4a2502559e3f62858a05a23cc11377b20275bb0
henry <misty0314@naver.com>
2025-08-26 00:43:57 +0900

    FlatList를 사용해 로그 데이터 출력

--- mini-game/screens/GameScreen.js
+++ mini-game/screens/GameScreen.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, StyleSheet, Alert, Text } from 'react-native';
+import { View, StyleSheet, Alert, Text, FlatList } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import Title from '../components/ui/Title';
@@ -85,7 +85,12 @@ function GameScreen({ userNumber, onGameOver }) {
                 </View>
             </Card>
             <View>
-                {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)}
+                {/* {guessRounds.map(guessRound => <Text key={guessRound}>{guessRound}</Text>)} */}
+                <FlatList
+                    data={guessRounds}
+                    renderItem={itemData => <Text>{itemData.item}</Text>}
+                    keyExtractor={(item) => item}
+                />
             </View>
         </View>
     );
